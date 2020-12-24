86964eacc2a9df6a3920fd7d62b3fc85c577ee1f
Ajin Asokan <ajin.panthayil@gmail.com>
2020-12-24 14:05:12 +0530

    update example project

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,81 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_curl/flutter_curl.dart';
-import 'package:path/path.dart' as p;
-import 'package:path_provider/path_provider.dart' as path;
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  Client c;
+class _MyAppState extends State<MyApp> {
+  Client curl;
+  String state = "Idle";
+  String statusCode = "NA";
+  String httpVersion = "NA";
+  String encoding = "NA";
 
   void init() async {
-    final cachePath = (await path.getTemporaryDirectory()).path;
-
-    c = Client(
-      altSvcCache: p.join(cachePath, "altsvc.txt"),
-      verbose: true,
-      interceptors: [
-        // HTTPCaching(),
-      ],
-    );
-
-    await c.init();
+    curl = Client(verbose: true);
+    await curl.init();
   }
 
   void _incrementCounter() async {
-    // Image.asset(name);
-    // CacheStorage.init();
-    // Client.libPath = "libcurl.so";
-
-    final res = await c.send(Request(
+    state = "Sending request";
+    setState(() {});
+    final res = await curl.send(Request(
       method: "GET",
       url: "https://ajinasokan.com/",
       headers: {},
@@ -85,10 +35,18 @@ class _MyHomePageState extends State<MyHomePage> {
 
     print("Status: ${res.statusCode}");
     print("HTTP: ${res.httpVersion}");
-    // res.headers.forEach((key, value) {
-    //   print("$key: $value");
-    // });
+    res.headers.forEach((key, value) {
+      print("$key: $value");
+      if (key == "content-encoding") {
+        encoding = value;
+      }
+    });
     // print(res.text());
+
+    statusCode = "${res.statusCode}";
+    httpVersion = "${res.httpVersion}";
+    state = "Idle";
+    setState(() {});
   }
 
   @override
@@ -99,59 +57,36 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   void dispose() {
-    c.dispose();
+    curl.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      home: Scaffold(
+        appBar: AppBar(title: Text("CURL Test")),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('State: $state'),
+              Text('Status code: $statusCode'),
+              Text('HTTP Version: $httpVersion'),
+              Text('Encoding: $encoding'),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _incrementCounter,
+          tooltip: 'Increment',
+          child: Icon(Icons.add),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
